e0880ceab7c112c3862d3b4955824a5ca042d6ab
claire <ikim318@aucklanduni.ac.nz>
2023-09-26 18:17:55 +1300

    Edit

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from plot import *
 
 if __name__ == "__main__":
 
-    # # benchmarking for ODE
+    # benchmarking for ODE
     plot_benchmark()
-    #
-    # # ODE model with initial parameter values
+
+    # ODE model with initial parameter values
     plot_suitable()
-    #
-    # # ODE model with improved parameter values from curve_fit
+
+    # ODE model with improved parameter values from curve_fit
     plot_improve()
 
 
