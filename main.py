01e569b99921195b566e46847b329d1eac8d055d
Jude Beullens <jbeu659@aucklanduni.ac.nz>
2023-10-04 12:46:48 +1300

    Graphs added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     #plot_benchmark()
 
     # ODE model with initial parameter values
-    plot_suitable()
+    #plot_suitable()
     #
     # # ODE model with improved parameter values from curve_fit
     plot_improve()
