126c52f0a3a71e33572b43fc699bdf661adc8f25
Jude Beullens <jbeu659@aucklanduni.ac.nz>
2023-10-03 17:16:20 +1300

    Plots saved

--- plot.py
+++ plot.py
@@ -248,11 +248,11 @@ def plot_suitable():
     [t, q, x_exact] = [load_data()[0], load_data()[1], load_data()[2]]
     # dqdt = find_dqdt(q)
     # TYPE IN YOUR PARAMETER ESTIMATE FOR a AND b HERE
-    a = 9.81 / (172225 * 0.2)
-    b = (10 ** -14 * 1000 * 172225) / (8.9 * 10 ** -8 * 415) * a
+    a = 9.81 / (160000 * 0.2)
+    b = (10 ** -14 * 1000 * 160000) / (8.9 * 10 ** -8 * 400) * a
     c = 0.01
-    # Guess x0 is 41.5 for average depth of reservoir
-    x0 = 41.5
+    # Guess x0 is 40 for average depth of reservoir
+    x0 = 40
     pars = [a, b, c, x0]
 
     # solve ODE with estimated parameters and plot
@@ -284,11 +284,11 @@ def plot_improve():
     # read in time and temperature data
     [t, x_exact] = [load_data()[0], load_data()[2]]
     # TYPE IN YOUR PARAMETER GUESS FOR a AND b HERE AS A START FOR OPTIMISATION
-    a = 9.81 / (172225 * 0.2)
-    b = (10 ** -14 * 1000 * 172225) / (8.9 * 10 ** -8 * 415) * a
+    a = 9.81 / (160000 * 0.2)
+    b = (10 ** -14 * 1000 * 160000) / (8.9 * 10 ** -8 * 400) * a
     c = 0.01
-    # Guess x0 is 41.5 for average depth of reservoir
-    x0 = 50
+    # Guess x0 is 40 for average depth of reservoir
+    x0 = 40
     pars_guess = [a, b, c, x0]
 
     # call to find out optimal parameters using guess as start
@@ -340,7 +340,7 @@ def plot_benchmark():
 
     # model values for benchmark analytic solution
     a = 1
-    b = 1
+    b = 10
     q = 1
     c = 1
     dqdt = 0
@@ -365,7 +365,7 @@ def plot_benchmark():
     t = np.array(t)
 
     #   TYPE IN YOUR ANALYTIC SOLUTION HERE
-    x_analytical = -((a * q) / b) * (1 - np.e ** (-b * t)) - c * dqdt + x0
+    x_analytical = -((a * q) / b) * (1 - np.e ** (-b * t)) - c * dqdt
 
     plot[0].plot(t, x_analytical, "r-", label="Analytical Solution")
     plot[0].legend(loc=1)
